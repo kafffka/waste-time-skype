1ca46a9780372017c37ab307c41a8b8e0767cf91
kafffka <mazurov.mark@gmail.com>
2016-12-17 21:19:26 +0300

    Add text in plot

--- waste-time-skype.py
+++ waste-time-skype.py
@@ -10,16 +10,16 @@ import numpy as np
 
 f = open('dialog.txt', 'r') # file with dialog from skype
 
-hours = 0
-minutes = 0
-seconds = 0
 all_hours = 0
-
 days = []
-minutes_per_day = []
+hours_per_day = []
 
 for i in f:
     if 'duration' in i:
+        hours = 0
+        minutes = 0
+        seconds = 0
+
         duration = i.split()[-2]
 
         day_from_dialog = datetime.strptime(i.split()[0], '[%d.%m.%Y')
@@ -29,22 +29,25 @@ for i in f:
         if len(duration.split(':')) == 2:
             duration = '0:' + duration
         time_list = duration.split(':')
-        hours += int(time_list[0])
-        minutes += int(time_list[1])
-        seconds += int(time_list[2])
+        hours = int(time_list[0])
+        minutes = int(time_list[1])
+        seconds = int(time_list[2]) + minutes*60 + hours*3600
 
         if not day_format in days:
-            all_hours += (hours*60*60+minutes*60+seconds)/60/60
-            minutes_per_day.append(all_hours)
+            all_hours += seconds/3600
+            hours_per_day.append(all_hours)
             days.append(day_format)
         else:
-            hours = 0
-            minutes = 0
-            seconds = 0
+            all_hours += seconds/3600
 
 
-plt.plot(days,minutes_per_day, label='Spent time (hours)')
+fig, ax = plt.subplots(1)
+plt.plot(days,hours_per_day, label='Spent time')
 plt.legend(framealpha=0.5)
 plt.ylabel('hours')
+props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
+textstr = '{} hours'.format(int(all_hours))
+ax.text(0.05, 0.95, textstr, transform=ax.transAxes, fontsize=14,
+        verticalalignment='top', bbox=props)
 plt.gcf().autofmt_xdate()
 plt.show()
