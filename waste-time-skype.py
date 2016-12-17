15d483dfc9a7bf38fad99b962f746fa3cc2bb9f6
kafffka <mazurov.mark@gmail.com>
2016-12-17 20:47:56 +0300

    Add matplotlib graph (hours, days)

--- waste-time-skype.py
+++ waste-time-skype.py
@@ -1,22 +1,49 @@
-f = open('dialog.txt', 'r') # file with dialog form skype
+'''
+Не считается последний день (в dialog не указывается дата, а только время)
+Не учитывается возможность того, что дата изначально задана правильно (%Y,%m,%d)
+'''
+
+
+from datetime import datetime, date
+import matplotlib.pyplot as plt
+import numpy as np
+
+f = open('dialog.txt', 'r') # file with dialog from skype
 
 hours = 0
 minutes = 0
 seconds = 0
+all_seconds = 0
+
+days = []
+minutes_per_day = []
 
 for i in f:
     if 'duration' in i:
-        time = i.split()[-2]
-        if len(time.split(':')) == 2:
-            time = '0:' + time
-        time_list = time.split(':')
+        duration = i.split()[-2]
+
+        day_from_dialog = datetime.strptime(i.split()[0], '[%d.%m.%Y')
+        y, m, d = map(int, day_from_dialog.strftime('%Y,%m,%d').split(','))
+        day_format = date(y, m, d)
+
+        if len(duration.split(':')) == 2:
+            duration = '0:' + duration
+        time_list = duration.split(':')
         hours += int(time_list[0])
         minutes += int(time_list[1])
         seconds += int(time_list[2])
 
-minutes += seconds // 60
-seconds = seconds % 60
-hours += minutes // 60
-minutes = minutes % 60
+        if not day_format in days:
+            all_seconds += (hours*60*60+minutes*60+seconds)/60/60
+            minutes_per_day.append(all_seconds)
+            days.append(day_format)
+        else:
+            hours = 0
+            minutes = 0
+            seconds = 0
 
-print('{} hours {} minutes {} seconds'.format(hours, seconds, minutes))
+plt.plot(days,minutes_per_day, label='Spent time (hours)')
+plt.legend(framealpha=0.5)
+plt.ylabel('hours')
+plt.gcf().autofmt_xdate()
+plt.show()
