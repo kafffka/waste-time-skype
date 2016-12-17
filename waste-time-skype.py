e7777a1f34e7f06d6804744f88ad7e894456da26
kafffka <mazurov.mark@gmail.com>
2016-12-17 20:56:01 +0300

    Fix

--- waste-time-skype.py
+++ waste-time-skype.py
@@ -13,7 +13,7 @@ f = open('dialog.txt', 'r') # file with dialog from skype
 hours = 0
 minutes = 0
 seconds = 0
-all_seconds = 0
+all_hours = 0
 
 days = []
 minutes_per_day = []
@@ -34,13 +34,14 @@ for i in f:
         seconds += int(time_list[2])
 
         if not day_format in days:
-            all_seconds += (hours*60*60+minutes*60+seconds)/60/60
-            minutes_per_day.append(all_seconds)
+            all_hours += (hours*60*60+minutes*60+seconds)/60/60
+            minutes_per_day.append(all_hours)
             days.append(day_format)
         else:
             hours = 0
             minutes = 0
             seconds = 0
+
 
 plt.plot(days,minutes_per_day, label='Spent time (hours)')
 plt.legend(framealpha=0.5)
